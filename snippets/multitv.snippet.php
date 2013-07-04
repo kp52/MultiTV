8d7ece4ca0dc5287a673f5bcb24492bd67a984b1
TimGS <timspe@gmail.com>
2013-07-04 11:47:25 +0100

    Version numbering (clipper-1.6)

--- snippets/multitv.snippet.php
+++ snippets/multitv.snippet.php
@@ -5,9 +5,9 @@
  * Transform template variables into a sortable multi item list.
  
  * @category 	snippet
- * @version 	1.5.2
+ * @version 	clipper-1.6
  * @license 	http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
- * @author		Jako (thomas.jakobi@partout.info)
+ * @author		Jako to version 1.5.2 and now the ClipperCMS community
  */
 return include(MODX_BASE_PATH.'assets/tvs/multitv/multitv.snippet.php');
 
